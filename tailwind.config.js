dc034c8a691cf2ab1e3cd585f6998d1e29221aee
billesonemilton <miltonsone12@gmail.com>
2024-08-04 19:45:26 +0100

    this is my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./main.js",
+    "./shoes.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./shoes.js",
+    "./Men.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./Men.js",
+    "./women.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./women.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [], 
+}
---
+++ women.js
@@ -0,0 +1,126 @@
+import "./style.scss";
+import gsap from "gsap";
+
+const items = document.querySelectorAll(".item");
+
+items.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    gsap.to(".right", {
+      duration: 2,
+      y: `-${index * 100}%`,
+      ease: "elastic.out(1,0.75)",
+    });
+    gsap.to(".left", {
+      duration: 2,
+      x: `${index * 345}px`,
+      ease: "elastic.out(1,0.75)",
+    });
+    if (index === 1) {
+      gsap.to(".item-2", {
+        duration: 2,
+        scale: 1,
+        ease: "elastic.out(1,0.85)",
+      });
+      gsap.to(".item-2", {
+        duration: 0,
+        filter: "blur(0)",
+      });
+      gsap.to(".item-1", {
+        duration: 2,
+        x: 100,
+      });
+    }
+    if (index === 2) {
+      gsap.to(".item-3", {
+        duration: 2,
+        scale: 1,
+        ease: "elastic.out(1,0.85)",
+      });
+      gsap.to(".item-3", {
+        duration: 0,
+        filter: "blur(0)",
+      });
+      gsap.to(".item-2", {
+        duration: 2,
+        x: 100,
+      });
+    }
+    if (index === 3) {
+      gsap.to(".item-4", {
+        duration: 2,
+        scale: 1,
+        ease: "elastic.out(1,0.85)",
+      });
+      gsap.to(".item-4", {
+        duration: 0,
+        filter: "blur(0)",
+      });
+      gsap.to(".item-3", {
+        duration: 2,
+        x: 100,
+      });
+    }
+  });
+});
+
+document.querySelector(".restart").addEventListener("click", () => {
+  gsap.to(".right", {
+    duration: 2,
+    y: 0,
+    ease: "elastic.out(1,0.75)",
+  });
+  gsap.to(".left", {
+    duration: 2,
+    x: 0,
+    ease: "elastic.out(1,0.75)",
+  });
+  gsap.to(".item-2", {
+    duration: 2,
+    scale: 0.8,
+    ease: "elastic.out(1,0.85)",
+  });
+  gsap.to(".item-2", {
+    duration: 0,
+    filter: "blur(12px)",
+  });
+  gsap.to(".item-1", {
+    duration: 2,
+    x: 0,
+  });
+  gsap.to(".item-3", {
+    duration: 2,
+    scale: 0.6,
+    ease: "elastic.out(1,0.85)",
+  });
+  gsap.to(".item-3", {
+    duration: 0,
+    filter: "blur(12px)",
+  });
+  gsap.to(".item-2", {
+    duration: 2,
+    x: 0,
+  });
+  gsap.to(".item-4", {
+    duration: 2,
+    scale: 0.4,
+    ease: "elastic.out(1,0.85)",
+  });
+  gsap.to(".item-4", {
+    duration: 0,
+    filter: "blur(12px)",
+  });
+  gsap.to(".item-3", {
+    duration: 2,
+    x: 0,
+  });
+});
+
+// sidebar.js
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleSidebar = document.querySelector('#toggle-sidebar');
+  const sidebar = document.querySelector('#sidebar');
+
+  toggleSidebar.addEventListener('click', function() {
+    sidebar.classList.toggle('hidden');
+  });
+});
